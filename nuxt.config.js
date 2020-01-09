41ca91858ca40e9ddfbb56266261c069f400514d
OlivierFortier <olivier_fortier@outlook.com>
2020-01-09 17:47:58 -0500

    changing names and content of head

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "Petits Soleils de la Pointe-de-l'île",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "Petits Soleils de la Pointe-de-l'île",
+        name: "Petits Soleils de la Pointe-de-l'île",
+        content: "Petits Soleils de la Pointe-de-l'île"
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
